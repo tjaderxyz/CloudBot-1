6ad398738ceb66ba23dad0389b71be3853a68d0d
tiredtyrant <tiredtyrant@users.noreply.github.com>
2020-05-23 16:07:21 -0300

    Add huecoin
    Something to replace cryptocurrency.py while it doesn't work with the free tier API

---
+++ plugins/huecoin.py
@@ -0,0 +1,43 @@
+from cloudbot import hook
+from cloudbot.util import colors
+import requests
+
+#https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=USD&include_24hr_change=true
+
+@hook.command("btc","bitcoin",autohelp=False)
+def huecoin(message, bot, conn):
+    params = {
+        'ids': 'bitcoin',
+        'vs_currencies': 'USD',
+        'include_24hr_change': 'true'
+    }
+    headers = {'User-Agent': bot.user_agent}
+    try:
+        r = requests.get("https://api.coingecko.com/api/v3/simple/price", params=params, headers=headers)
+        r.raise_for_status()
+        response = r.json()
+    except Exception:
+        return("rip .btc")
+        print(response)
+        raise
+        
+    #example:
+    #{
+    #  'bitcoin': {
+    #    'usd': 9173.02, 
+    #    'usd_24h_change': -0.24865295786135225
+    #    }
+    #}
+    value = colors.parse('$(orange)${:,.2f}$(clear)'.format(response['bitcoin']['usd']))
+    change = response['bitcoin']['usd_24h_change'];
+    fchange = float(change);
+    
+    if fchange < 0:
+        changestr = colors.parse("$(dark_red){}%$(clear)".format(change))
+    elif fchange > 0:
+        changestr = colors.parse("$(dark_red){}%$(clear)".format(change))
+    else:
+        changestr = "{}%".format(change)
+        
+    return "1 BTC: {} USD - {}".format(value, changestr);
+    
